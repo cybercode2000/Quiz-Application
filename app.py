6cf8bc38473866fc43b596e66fb54ff52f08c03c
Awopetu Rasheed Oluwadamilare <47466539+cybercode2000@users.noreply.github.com>
2022-08-21 18:31:04 +0100

    update

---
+++ app.py
@@ -0,0 +1,8 @@
+from cProfile import label
+import tkinter
+window = tkinter.Tk()
+window.title("QUIZ APPLICATION")
+
+label = tkinter.Label(window, text ="hello world nigeria")
+
+window.mainloop()
